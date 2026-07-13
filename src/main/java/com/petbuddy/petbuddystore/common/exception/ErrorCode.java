36c42d05a1ccc3d775302f53f3d047abba7da13e
Nguyễn Phùng Anh Tuấn <nguyenphunganhtuan2004@gmail.com>
2026-07-13 09:41:03 +0700

    fix: remove duplicate error code promotion

--- src/main/java/com/petbuddy/petbuddystore/common/exception/ErrorCode.java
+++ src/main/java/com/petbuddy/petbuddystore/common/exception/ErrorCode.java
@@ -255,9 +255,6 @@ public enum ErrorCode {
 
     PROMOTION_PRODUCT_NOT_FOUND(5504, "Product in promotion details not found", HttpStatus.BAD_REQUEST),
     PROMOTION_STATUS_REQUIRED(5505, "Promotion status is required", HttpStatus.BAD_REQUEST),
-    PROMOTION_NAME_REQUIRED(5506, "Promotion name is required", HttpStatus.BAD_REQUEST),
-    PROMOTION_START_DATE_REQUIRED(5507, "Start date is required", HttpStatus.BAD_REQUEST),
-    PROMOTION_END_DATE_REQUIRED(5508, "End date is required", HttpStatus.BAD_REQUEST),
     PROMOTION_DISCOUNT_TYPE_REQUIRED(5509, "Discount type is required", HttpStatus.BAD_REQUEST),
     PROMOTION_DISCOUNT_VALUE_REQUIRED(5510, "Discount value is required", HttpStatus.BAD_REQUEST),
     PROMOTION_PRODUCT_REQUIRED(5511, "Product is required", HttpStatus.BAD_REQUEST),
